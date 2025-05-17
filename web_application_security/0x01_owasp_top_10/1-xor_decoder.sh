0e16a451d8cc7d892633be6901a919938a9fa7df
Abraham Vidal <abrahamvidalcastillo2@gmail.com>
2025-05-17 18:38:13 -0400

    decodes xor pw encoded in base 64

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -22,7 +22,7 @@ fi
 for ((i = 0; i < ${#decoded}; i++)); do
     byte=$(printf '%d' "'${decoded:$i:1}")
     printf "\\$(printf '%03o' "$((byte ^ 0x7F))")"
-done
+done | tr '[:upper:]' '[:lower:]'
 
 echo
 
